e160131707ee74f6dee53aa00363af8f8f1dfd83
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-12-30 07:31:40 +0600

    handles the remove functionalities

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,17 +6,32 @@ import "./Countries.css";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
   const [visitedCountries, setVisitedCountries] = useState([]);
+  const [deletedList, setDeletedList] = useState([]);
+
+  console.log(deletedList);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
-  });
+  }, []);
 
   /* Handle Visited Countries */
   const handleVisitedCountries = (country) => {
     const newVisitedCountries = [...visitedCountries, country];
     setVisitedCountries(newVisitedCountries);
+  };
+
+  const handleRemove = (desh) => {
+    const remaining = visitedCountries.filter(
+      (country) => country.cca3 !== desh.cca3
+    );
+    setVisitedCountries(remaining);
+    const selectOne = visitedCountries.find(
+      (country) => country.cca3 === desh.cca3
+    );
+    console.log(selectOne);
+    setDeletedList([...deletedList, selectOne]);
   };
 
   return (
@@ -27,9 +42,21 @@ const Countries = () => {
         <h4>Visited Countries: {visitedCountries.length}</h4>
         <ul>
           {visitedCountries.map((country) => (
-            <li key={country.cca3}>{country.name.common}</li>
+            <div key={country.cca3} style={{ margin: "1rem" }}>
+              <li>{country.name.common}</li>
+              <button onClick={() => handleRemove(country)}>Delete</button>
+            </div>
           ))}
         </ul>
+
+        <div>
+          <h2>Deleted List: {deletedList.length}</h2>
+          {deletedList.map((country) => (
+            <div key={country.cca3}>
+              <p>{country.name.common}</p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Countries */}
--- src/components/CountryDetail/CountryDetail.jsx
+++ src/components/CountryDetail/CountryDetail.jsx
@@ -1,9 +1,8 @@
 import CountryData from "../CountryData/CountryData";
 
 const CountryDetail = (props) => {
-
-  const {country} = props
-  console.log(country);
+  const { country } = props;
+  // console.log(country);
   return (
     <div>
       <h3>Country Region: {country.region}</h3>
